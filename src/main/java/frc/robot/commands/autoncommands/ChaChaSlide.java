47ce8f614125752fae4611ff31b821c634c17d01
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2019-09-20 15:14:01 -0400

    Added Cha Cha Slide

---
+++ src/main/java/frc/robot/commands/autoncommands/ChaChaSlide.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.autoncommands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.autoncommands.DriveStraight.DDirection;
+
+public class ChaChaSlide extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public ChaChaSlide() {
+    // isn't this the only reason to have a holonomic drivetrain?
+
+    addSequential(new DriveStraight(1, 0.8, DDirection.RIGHT));
+    Timer.delay(2);
+    addSequential(new DriveStraight(1, 0.8, DDirection.LEFT));
+    Timer.delay(2);
+    addSequential(new DriveStraight(1, 0.8, DDirection.BACKWARD));
+  }
+}
