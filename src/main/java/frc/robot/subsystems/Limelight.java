913e5dd56cd5590fa1a0ffb09326318088440e06
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2019-10-22 17:46:17 -0400

    Changed limelight getters and setters

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,59 +7,44 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * Add your docs here.
+ * Limelight getters and setters
  */
 public class Limelight extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  private static NetworkTable table;
-  private NetworkTableEntry tx, ty, ta, tv;
-
   public Limelight() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-
     setDriving();
   }
 
   public double getX() {
-    tx = table.getEntry("tx");
-    return tx.getDouble(0.0);
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
   }
 
   public double getY() {
-    ty = table.getEntry("ty");
-    return ty.getDouble(0.0);
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
   }
 
   public void setTracking() {
-    table.getEntry("ledMode").setNumber(3);
-    table.getEntry("camMode").setNumber(0);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
   }
 
   public void setDriving() {
-    table.getEntry("ledMode").setNumber(1);
-    table.getEntry("camMode").setNumber(1);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1);
   }
 
   public double getArea() {
-    ta = table.getEntry("ta");
-    return ta.getDouble(0.0);
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
   }
 
   public boolean getAvalible() {
-    tv = table.getEntry("tv");
-    return tv.getDouble(0.0) == 1;
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1;
   }
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
   }
 }
