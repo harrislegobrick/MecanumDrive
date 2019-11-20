a66d7fd41934339d56685787e6e360c4b2ce8b13
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2019-11-19 19:58:23 -0500

    Added turret left and right control

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,11 +10,13 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.POVButton;
 import frc.robot.commands.CalibrateLimelight;
 import frc.robot.commands.MecanumDriveWithStick;
+import frc.robot.commands.RotateLLTurret;
 import frc.robot.commands.MecanumDriveWithStick.Orientation;
+import frc.robot.commands.RotateLLTurret.Direction;
 import frc.robot.commands.autoncommands.LimelightTrackToTarget;
-
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -23,25 +25,28 @@ import frc.robot.commands.autoncommands.LimelightTrackToTarget;
 public class OI {
   private static Joystick stick = new Joystick(RobotMap.JOY_PORT);
   private static Button fieldOriented, robotOriented, trackToTarget, calibrateLimelight;
-
+  private static POVButton rotateL, rotateR;
 
   public static void init() {
     robotOriented = new JoystickButton(stick, 3);
     fieldOriented = new JoystickButton(stick, 4);
-    trackToTarget = new JoystickButton(stick, 5);
+    trackToTarget = new JoystickButton(stick, 2);
     calibrateLimelight = new JoystickButton(stick, 6);
-    
-   
+    rotateL = new POVButton(stick, 270);
+    rotateR = new POVButton(stick, 90);
+
     fieldOriented.whenPressed(new MecanumDriveWithStick(Orientation.FIELD));
     robotOriented.whenPressed(new MecanumDriveWithStick(Orientation.ROBOT));
     trackToTarget.whileHeld(new LimelightTrackToTarget());
     calibrateLimelight.toggleWhenPressed(new CalibrateLimelight());
+    rotateL.whileHeld(new RotateLLTurret(Direction.CCW));
+    rotateR.whileHeld(new RotateLLTurret(Direction.CW));
   }
 
   public double getJoyY() {
     return stick.getY();
   }
-  
+
   public double getJoyX() {
     return stick.getX();
   }
